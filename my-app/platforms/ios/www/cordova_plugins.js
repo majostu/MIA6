63b1cc09c1c9d85fe764e748192db03a9a9386b7
Marijn Stuyfzand <majostu@hotmail.com>
2015-05-12 11:53:36 +0200

    default new app

--- my-app/platforms/ios/www/cordova_plugins.js
+++ my-app/platforms/ios/www/cordova_plugins.js
@@ -3,7 +3,7 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.1-dev"
+    "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
