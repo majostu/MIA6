d70b6c26c7a0fc6cabc28460663282270d68350f
Marijn Stuyfzand <majostu@hotmail.com>
2015-05-29 17:36:27 +0200

    google maps with get location

--- handyfriendly/platforms/ios/www/js/index.js
+++ handyfriendly/platforms/ios/www/js/index.js
@@ -12,46 +12,125 @@ var app = {
     },
     // deviceready Event Handler
     //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    	
     }
+ 
 };
 
 app.initialize();
 
-var module = ons.bootstrap('handyfriendly', ['onsen']);
+var module = angular.module('handyfriendly', ['onsen']); //'uiGmapgoogle-maps'
+
 module.controller('AppController', function($scope) { 
 	ons.ready(function() {
 	// Init code here
 	});
 });
 
-module.controller('MapController', function($scope) {
+/*module.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyBeyepY1W5MQtdovxCS_6SWYouRGobAfz4',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+    });
+});*/
+
+module.controller('MapController', function($scope, $compile) {
 	ons.ready(function() {
-	// Init code here
+				
+	    /*uiGmapGoogleMapApi.then(function(maps) {
+	        $scope.map = { 
+		        center: { 
+			        latitude: 51.441642, 
+			        longitude: 5.469722 
+			    }, 
+				zoom: 8,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+	        console.log('map: ', maps);
+	    });*/
+	    
+	    //function initialize() {
+		    	    		    
+        	var myLatlng = new google.maps.LatLng(51.441642, 5.469722);
+        
+			var mapOptions = {
+				center: myLatlng,
+				zoom: 16,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+        	};
+			var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+        
+			//Marker + infowindow + angularjs compiled ng-click
+			var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+			var compiled = $compile(contentString)($scope);
+
+			var infowindow = new google.maps.InfoWindow({
+				content: compiled[0]
+        	});
+
+			var marker = new google.maps.Marker({
+				position: myLatlng,
+				map: map,
+				title: 'Uluru (Ayers Rock)'
+        	});
+
+			google.maps.event.addListener(marker, 'click', function() {
+				infowindow.open(map,marker);
+        	});
+
+			$scope.map = map;
+				
+      	//}
+	  	
+	  	//google.maps.event.addDomListener(window, 'load', initialize);
+      
+	  	$scope.centerOnMe = function() {
+        	if(!$scope.map) {
+				return;
+        	}
+
+			/*$scope.loading = $ionicLoading.show({
+				content: 'Getting current location...',
+				showBackdrop: false
+        	});*/
+
+			navigator.geolocation.getCurrentPosition(function(pos) {
+				
+				$scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+				
+				//$scope.loading.hide();
+        	}, function(error) {
+				alert('Unable to get location: ' + error.message);
+        	});
+      	};
+      
+	  	$scope.clickTest = function() {
+        	alert('Example of infowindow with ng-click')
+      	};
+			
 	});
 });
 
+
+	
+	
+	
+	
+	
+	
+	
+	
+
 module.controller('FavoritesController', function($scope) {
 	ons.ready(function() {
-	
-		$scope.content = {
-        	name: 'favorites'
-      	};
-	
+
+		$scope.fav_bar = {
+			name: "favorites"	
+		};
+		
 	});
 });
 
@@ -66,6 +145,3 @@ module.controller('SettingsController', function($scope) {
 	// Init code here
 	});
 });
-
-
-
